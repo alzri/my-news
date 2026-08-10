d7c94ab15d9bd7a1af7a329cbeda9f7dc898dbb4
azrinka <alzrinka1@gmail.com>
2026-08-10 17:23:00 +0200

    text fix

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,22 +1,20 @@
+import React, { forwardRef } from 'react';
 import { ITextProps } from './Text.types';
 import clsx from 'clsx';
 import styles from './Text.module.scss';
 
-export const Text = ({
-  ref,
-  component = 'p',
-  size,
-  color = 'primary',
-  children,
-  className,
-  ...rest
-}: ITextProps) => {
-  const TagName = component;
-  const classNames = clsx('text', className, styles[size], styles[color]);
+export const Text = forwardRef<HTMLElement, ITextProps>(
+  ({ component = 'p', size, color = 'primary', children, className, ...rest }, ref) => {
+    const TagName = component as React.ElementType;
+    const classNames = clsx('text', className, styles[size], styles[color]);
+    const domProps = rest as Record<string, unknown>;
 
-  return (
-    <TagName ref={ref} className={classNames} {...rest}>
-      {children}
-    </TagName>
-  );
-};
+    return React.createElement(
+      TagName,
+      { ref: ref as React.Ref<HTMLElement>, className: classNames, ...domProps },
+      children
+    );
+  }
+);
+
+Text.displayName = 'Text';
